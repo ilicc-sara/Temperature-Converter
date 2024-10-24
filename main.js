5b072d4426f7112b441aa63e39003c05a0895ec8
Sara <sarailic431@gmail.com>
2024-10-24 18:02:09 +0200

    adding and modify

--- main.js
+++ main.js
@@ -27,18 +27,18 @@ inputKel.addEventListener("input", function (e) {
 });
 
 form.addEventListener("keydown", function (e) {
-  if (e.key === "Enter" && e.target.className === `${"celsius"}`) {
+  if (e.key === "Enter" && e.target.className === `celsius`) {
     inputFar.value = (cels * (9 / 5) + 32).toFixed(2);
     inputKel.value = (cels + 273.15).toFixed(2);
     //console.log(e.target.className);
   }
 
-  if (e.key === "Enter" && e.target.className === `${"farenheit"}`) {
+  if (e.key === "Enter" && e.target.className === `farenheit`) {
     inputCels.value = (((far - 32) * 5) / 9).toFixed(2);
     inputKel.value = (((far - 32) * 5) / 9 + 273.15).toFixed(2);
   }
 
-  if (e.key === "Enter" && e.target.className === `${"kelvin"}`) {
+  if (e.key === "Enter" && e.target.className === `kelvin`) {
     inputCels.value = (kel - 273.15).toFixed(2);
     inputFar.value = ((kel - 273.15) * 1.8 + 32).toFixed(2);
   }
